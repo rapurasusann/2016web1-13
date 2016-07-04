9132881964ca2292024e3e8575c38a4660130cd6
rapurasusann <rapurasusann@yahoo.co.jp>
2016-07-04 17:29:28 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,7 +1,7 @@
 var separate_time = function(time){
 var sec = time.getSeconds();
 var min = time.getMinutes();
-var hours = time getHours();
+var hours = time.getHours();
 var days= time.getDate();
 var month = time.getMonth();
 var year = time.getFullYear();
